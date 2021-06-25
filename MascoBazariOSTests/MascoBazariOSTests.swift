14e37421fe41afd429040b6e3f167b8c8f48c207
Subroto <subroto_mohonto>
2021-06-25 19:59:56 +0600

    Initial Commit

---
+++ MascoBazariOSTests/MascoBazariOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  MascoBazariOSTests.swift
+//  MascoBazariOSTests
+//
+//  Created by masco bazar on 25/6/21.
+//
+
+import XCTest
+@testable import MascoBazariOS
+
+class MascoBazariOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
