8350d26d8c9f106c4c40acb0cfbccebffbd0822d
Subroto <subroto_mohonto>
2021-07-03 12:29:46 +0600

    Side Menu Structure

--- MascoBazariOS/ViewController.swift
+++ MascoBazariOS/ViewController.swift
@@ -18,11 +18,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchingInnerView: UIView!
     @IBOutlet weak var searchingFullView: UIView!
     
+    @IBOutlet weak var leadingConstantForMenuView: NSLayoutConstraint!
+    @IBOutlet weak var menuView: UIView!
+    
+    @IBOutlet weak var menuBackgroundView: UIView!
+    
+    private var isMenuShown:Bool = false
+    private var beginPoint:CGFloat = 0.0
+    private var difference:CGFloat = 0.0
+    
+    var sideMenuViewController : SideMenuViewController?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         cornerRoundView()
+        self.menuBackgroundView.isHidden = true
     }
     
     
@@ -42,7 +54,29 @@ class ViewController: UIViewController {
     @IBAction func searchingAllClearBtn(_ sender: UIButton) {
     }
     
+    @IBAction func menuBtn(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.1) {
+            self.leadingConstantForMenuView.constant = 10
+            self.view.layoutIfNeeded()
+        } completion: { (status) in
+            self.menuBackgroundView.alpha = 0.75
+            self.menuBackgroundView.isHidden = false
+            UIView.animate(withDuration: 0.1) {
+                self.leadingConstantForMenuView.constant = 0
+                self.view.layoutIfNeeded()
+            } completion: { (status) in
+                self.isMenuShown = true
+            }
+
+        }
+
+        self.menuBackgroundView.isHidden = false
+    }
     
+    @IBAction func topped(_ sender: Any) {
+        self.hideMenuView()
+    }
     func cornerRoundView(){
         searchingView.clipsToBounds = true
         searchingView.layer.cornerRadius = 23
@@ -55,6 +89,79 @@ class ViewController: UIViewController {
         footerView.clipsToBounds = true
         footerView.layer.cornerRadius = 30
         footerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    
+    private func hideMenuView()
+    {
+        UIView.animate(withDuration: 0.1) {
+            self.leadingConstantForMenuView.constant = 10
+            self.view.layoutIfNeeded()
+        } completion: { (status) in
+            self.menuBackgroundView.alpha = 0.0
+            UIView.animate(withDuration: 0.1) {
+                self.leadingConstantForMenuView.constant = -280
+                self.view.layoutIfNeeded()
+            } completion: { (status) in
+                self.menuBackgroundView.isHidden = true
+                self.isMenuShown = false
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (isMenuShown)
+        {
+             if let touch = touches.first
+            {
+                let location = touch.location(in: menuBackgroundView)
+                beginPoint = location.x
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (isMenuShown)
+        {
+            if let touch = touches.first
+            {
+                let location = touch.location(in: menuBackgroundView)
+                
+                let differenceFromBeginPoint = beginPoint - location.x
+                
+                if (differenceFromBeginPoint>0 || differenceFromBeginPoint<280)
+                {
+                    difference = differenceFromBeginPoint
+                    self.leadingConstantForMenuView.constant = -differenceFromBeginPoint
+                    self.menuBackgroundView.alpha = 0.75-(0.75*differenceFromBeginPoint/280)
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (isMenuShown)
+        {
+            if (difference>140)
+            {
+                UIView.animate(withDuration: 0.1) {
+                    self.leadingConstantForMenuView.constant = -290
+                } completion: { (status) in
+                    self.menuBackgroundView.alpha = 0.0
+                    self.isMenuShown = false
+                    self.menuBackgroundView.isHidden = true
+                }
+            }
+            else{
+                UIView.animate(withDuration: 0.1) {
+                    self.leadingConstantForMenuView.constant = -10
+                } completion: { (status) in
+                    self.menuBackgroundView.alpha = 0.75
+                    self.isMenuShown = true
+                    self.menuBackgroundView.isHidden = false
+                }
+            }
+        }
     }
 }
 
